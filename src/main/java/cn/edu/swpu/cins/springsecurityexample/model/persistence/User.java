63e0aaedab765eab2e96f06283f164bbea200745
bohenmian <398206896@qq.com>
2017-11-20 11:26:01 +0800

    add user model and repository

---
+++ src/main/java/cn/edu/swpu/cins/springsecurityexample/model/persistence/User.java
@@ -0,0 +1,58 @@
+package cn.edu.swpu.cins.springsecurityexample.model.persistence;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "username", length = 25)
+    @NotNull
+    @Size(min = 6, max = 15)
+    private String username;
+
+    @Column(name = "password", length = 25)
+    @NotNull
+    @Size(min = 6, max = 15)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/cn/edu/swpu/cins/springsecurityexample/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package cn.edu.swpu.cins.springsecurityexample.repository;
+
+import cn.edu.swpu.cins.springsecurityexample.model.persistence.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findAll();
+}
