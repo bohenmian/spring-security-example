8957c9f8f86db3347398ef087923fc046d0ea4a8
bohenmian <398206896@qq.com>
2017-12-09 19:27:57 +0800

    add Browser controller

--- src/main/java/cn/edu/swpu/cins/springsecurityexample/controller/BrowserSecurityController.java
+++ src/main/java/cn/edu/swpu/cins/springsecurityexample/controller/BrowserSecurityController.java
@@ -1,8 +1,10 @@
 package cn.edu.swpu.cins.springsecurityexample.controller;
 
+import cn.edu.swpu.cins.springsecurityexample.config.properties.SecurityProperties;
 import cn.edu.swpu.cins.springsecurityexample.model.http.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
@@ -26,6 +28,13 @@ public class BrowserSecurityController {
     private RequestCache requestCache = new HttpSessionRequestCache();
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
+    private SecurityProperties securityProperties;
+
+    @Autowired
+    public BrowserSecurityController(SecurityProperties securityProperties) {
+        this.securityProperties = securityProperties;
+    }
+
     @RequestMapping("/authentication/require")
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public Message requireAuthentication(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -34,9 +43,9 @@ public class BrowserSecurityController {
             String target = savedRequest.getRedirectUrl();
             logger.info("引发跳转的请求是：" + target);
             if (StringUtils.endsWithIgnoreCase(target, ".html")) {
-                redirectStrategy.sendRedirect(request, response, "");
+                redirectStrategy.sendRedirect(request, response, securityProperties.getBrowserProperties().getLoginPage());
             }
         }
-        return new Message("没有权限");
+        return new Message("没有权限,请登录");
     }
 }
