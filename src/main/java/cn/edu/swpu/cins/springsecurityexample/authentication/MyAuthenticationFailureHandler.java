9366f9634cd058a0d41a216bfd161d6aadd87fa7
bohenmian <398206896@qq.com>
2017-12-10 20:10:37 +0800

    review the code

--- src/main/java/cn/edu/swpu/cins/springsecurityexample/authentication/MyAuthenticationFailureHandler.java
+++ src/main/java/cn/edu/swpu/cins/springsecurityexample/authentication/MyAuthenticationFailureHandler.java
@@ -1,12 +1,15 @@
 package cn.edu.swpu.cins.springsecurityexample.authentication;
 
+import cn.edu.swpu.cins.springsecurityexample.config.properties.SecurityProperties;
+import cn.edu.swpu.cins.springsecurityexample.enums.LoginType;
+import cn.edu.swpu.cins.springsecurityexample.model.http.Message;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
@@ -15,9 +18,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class MyAuthenticationFailureHandler implements AuthenticationFailureHandler {
+public class MyAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     private Logger logger = LoggerFactory.getLogger(MyAuthenticationFailureHandler.class);
+
+    @Autowired
+    private SecurityProperties securityProperties;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -26,8 +32,13 @@ public class MyAuthenticationFailureHandler implements AuthenticationFailureHand
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException exception) throws IOException, ServletException {
         logger.info("登录失败");
-        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        response.setContentType("application/json; charset = UTF-8");
-        response.getWriter().write(objectMapper.writeValueAsString(exception));
+        if (LoginType.JSON.equals(securityProperties.getBrowserProperties().getLoginType())) {
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            response.setContentType("application/json; charset = UTF-8");
+            response.getWriter().write(objectMapper.writeValueAsString(new Message(exception.getMessage())));
+        } else {
+            super.onAuthenticationFailure(request, response, exception);
+        }
+
     }
 }
--- src/main/java/cn/edu/swpu/cins/springsecurityexample/authentication/MyAuthenticationSuccessHandler.java
+++ src/main/java/cn/edu/swpu/cins/springsecurityexample/authentication/MyAuthenticationSuccessHandler.java
@@ -1,11 +1,13 @@
 package cn.edu.swpu.cins.springsecurityexample.authentication;
 
+import cn.edu.swpu.cins.springsecurityexample.config.properties.SecurityProperties;
+import cn.edu.swpu.cins.springsecurityexample.enums.LoginType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
@@ -14,10 +16,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+public class MyAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     private Logger logger = LoggerFactory.getLogger(MyAuthenticationSuccessHandler.class);
 
+    @Autowired
+    private SecurityProperties securityProperties;
     @Autowired
     private ObjectMapper objectMapper;
 
@@ -25,7 +29,12 @@ public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHand
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
         logger.info("登录成功");
-        response.setContentType("application/json; charset = UTF-8");
-        response.getWriter().write(objectMapper.writeValueAsString(authentication));
+        if (LoginType.JSON.equals(securityProperties.getBrowserProperties().getLoginType())) {
+            response.setContentType("application/json; charset = UTF-8");
+            response.getWriter().write(objectMapper.writeValueAsString(authentication));
+        } else {
+            super.onAuthenticationSuccess(request, response, authentication);
+        }
+
     }
 }
