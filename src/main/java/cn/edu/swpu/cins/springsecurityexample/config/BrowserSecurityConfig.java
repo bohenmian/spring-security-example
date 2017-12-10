4d0a7dc157ee103f1ad6102732749046a9182caa
bohenmian <398206896@qq.com>
2017-12-10 20:43:43 +0800

    add browser login type and config

--- src/main/java/cn/edu/swpu/cins/springsecurityexample/config/BrowserSecurityConfig.java
+++ src/main/java/cn/edu/swpu/cins/springsecurityexample/config/BrowserSecurityConfig.java
@@ -1,6 +1,8 @@
 package cn.edu.swpu.cins.springsecurityexample.config;
 
+import cn.edu.swpu.cins.springsecurityexample.authentication.MyAuthenticationFailureHandler;
 import cn.edu.swpu.cins.springsecurityexample.authentication.MyAuthenticationSuccessHandler;
+import cn.edu.swpu.cins.springsecurityexample.config.filter.ValidateCodeFilter;
 import cn.edu.swpu.cins.springsecurityexample.config.properties.SecurityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,17 +11,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private SecurityProperties securityProperties;
     private MyAuthenticationSuccessHandler authenticationSuccessHandler;
+    private MyAuthenticationFailureHandler authenticationFailureHandler;
 
     @Autowired
-    public BrowserSecurityConfig(SecurityProperties securityProperties, MyAuthenticationSuccessHandler authenticationSuccessHandler) {
+    public BrowserSecurityConfig(SecurityProperties securityProperties, MyAuthenticationSuccessHandler authenticationSuccessHandler,
+                                 MyAuthenticationFailureHandler authenticationFailureHandler) {
         this.securityProperties = securityProperties;
         this.authenticationSuccessHandler = authenticationSuccessHandler;
+        this.authenticationFailureHandler = authenticationFailureHandler;
     }
 
     @Bean
@@ -27,16 +33,25 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public ValidateCodeFilter validateCodeFilterBeam() {
+        ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
+        validateCodeFilter.setAuthenticationFailureHandler(authenticationFailureHandler);
+        return new ValidateCodeFilter();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.addFilterBefore(validateCodeFilterBeam(), UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable()
                 .formLogin()
                 .loginPage("/authentication/require")
                 .loginProcessingUrl("/authentication/form")
                 .successHandler(authenticationSuccessHandler)
+                .failureHandler(authenticationFailureHandler)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/authentication/require", securityProperties.getBrowserProperties().getLoginPage())
+                .antMatchers("/authentication/require", securityProperties.getBrowserProperties().getLoginPage(), "/user/code/image")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
--- src/main/java/cn/edu/swpu/cins/springsecurityexample/config/properties/BrowserProperties.java
+++ src/main/java/cn/edu/swpu/cins/springsecurityexample/config/properties/BrowserProperties.java
@@ -1,8 +1,12 @@
 package cn.edu.swpu.cins.springsecurityexample.config.properties;
 
+import cn.edu.swpu.cins.springsecurityexample.enums.LoginType;
+
 public class BrowserProperties {
 
     private String loginPage = "/signIn.html";
+
+    private LoginType loginType = LoginType.JSON;
 
     public String getLoginPage() {
         return loginPage;
@@ -10,5 +14,13 @@ public class BrowserProperties {
 
     public void setLoginPage(String loginPage) {
         this.loginPage = loginPage;
+    }
+
+    public LoginType getLoginType() {
+        return loginType;
+    }
+
+    public void setLoginType(LoginType loginType) {
+        this.loginType = loginType;
     }
 }
